dfcec854a923d15b4c6a4da8c7fcbb5007a21c21
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-11 17:35:48 +0300

    mongo db client adapter was installed and connected

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
+  adapter: MongoDBAdapter({ clientPromise }),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
