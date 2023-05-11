23d42d2c75b8c9c568c7fee974f0e06200d135f1
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-11 21:16:26 +0300

    nav component was added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -19,15 +20,18 @@ export default function Home() {
   }
 
   return (
-    <div className="flex items-center w-screen h-screen bg-blue-900">
-      <div className="text-center w-full">
-        <h1>Logged In as {session.user.email}</h1>
-        <button
-          className="bg-white p-2 px-4 rounded-lg"
-          onClick={() => signOut()}
-        >
-          Logout
-        </button>
+    <div className="flex w-screen h-screen bg-blue-900">
+      <Nav />
+      <div className="px-8 py-4 m-4 ml-0 rounded-xl flex-grow items-center bg-gray-50">
+        <div className="flex gap-4 justify-end">
+          <h1 className="text-2xl font-semibold">{session.user.name}</h1>
+          <button
+            className="border-blue-800 border-2 py-2 px-4 rounded-lg text-blue-800/90 hover:bg-blue-800/90 hover:border-blue-400/50 hover:text-white/90"
+            onClick={() => signOut()}
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
